352f7ba98c2763a055d56e229d662cc3c37918f8
Dempsey845 <dempseynewton1@gmail.com>
2026-08-10 11:54:11 +0100

    Added the ice vapour particle effect to the shoe box.

--- Item/Items/ShoeBox/shoe_box.gd
+++ Item/Items/ShoeBox/shoe_box.gd
@@ -2,9 +2,12 @@ class_name ShoeBox
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var ice_vapour_particles: GPUParticles3D = $IceVapourParticles
 
 func open_lid():
     animation_player.play_backwards("close_lid")
+    ice_vapour_particles.emitting = true
 
 func close_lid():
     animation_player.play("close_lid")
+    ice_vapour_particles.emitting = false
